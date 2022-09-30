c63a744772e27675b5f0a46776cb58b4044421d9
Mohammad Keshtehgar <mk.qeshmair@gmail.com>
2022-09-30 13:20:22 +0330

    add Hydrated_bloc and save state at appdirectory

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:bloc/bloc.dart';
 import 'package:counter_bloc/app.dart';
 import 'package:counter_bloc/counter_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory());
   Bloc.observer = CounterObserver();
-  runApp(const CounterApp());
+  HydratedBlocOverrides.runZoned(() => runApp(const CounterApp()),
+      storage: storage);
 }
